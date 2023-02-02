51657210c431bec7885c349ec46750bcedb48b17
Reid Miller <reid.s.miller@gmail.com>
2023-02-02 14:20:11 -0700

    Add tests for jungle_beat_spec

---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require './lib/linked_list'
+require './lib/node'
+
+RSpec.descirbe JungleBeat do
+  describe '#initialize' do
+    it 'exists' do
+      jb = JungleBeat.new
+
+      expect(jb).to be_instance_of(JungleBeat)
+      expect(jb.list.head).to be nil
+    end
+
+    it 'can append multiple strings into link chain' do
+      jb = JungleBeat.new
+
+      jb.append("deep doo ditt")
+
+      expect(jb.list.head.data).to eq("deep")
+      expect(jb.list.head.next_node.data)("doo")
+    end
+
+    it 'can append even further' do
+      jb = JungleBeat.new
+
+      jb.append("deep doo ditt")
+      jb.append("woo hoo shu")
+
+      expect(jb.count).to eq(6)
+    end
+  end
+end
