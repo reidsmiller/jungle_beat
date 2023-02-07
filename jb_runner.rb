4c65ef278f970bb12a194f1f09f178d0de664ab6
Reid Miller <reid.s.miller@gmail.com>
2023-02-06 18:53:30 -0700

    fix: remove runinit and move into jb_runner

--- jb_runner.rb
+++ jb_runner.rb
@@ -6,12 +6,26 @@ require './lib/runinit'
 p '-----------Hello intrepid beat-boxer!------------'
 p 'Welcome to the Beat-O-Matic 40023'
 p 'I have a few questions to set up your sweet beats!'
-p '__________________________________________________'
+p '-------------------------------------------------'
 
 p 'First, what phrase would you like to intone?'
 
-runinit = RunInit.new(gets.chomp)
+jb = JungleBeat.new(gets.chomp)
 
-p 'Awesome!'
+p "-------------------------------------------------"
+p "If you would like to re-enter your phrase, enter 'yes', if you are satisfied, 'no'"
+
+ans = (gets.chomp)
+if ans == "yes"
+  p "Enter your new phrase"
+  jb = JungleBeat.new(gets.chomp)
+else
+  p "Great!"
+end
+
+p 'How many times would you like the computer to read your killer beats?'
+
+jb.loop(gets.chomp.to_i)
 
 require 'pry'; binding.pry
+
